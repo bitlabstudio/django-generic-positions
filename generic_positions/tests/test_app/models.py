778b373460dde55d32cabcd5b9125b37d04a4e5e
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2013-03-20 09:14:06 +0100

    added ordering to custom admin

--- generic_positions/tests/test_app/models.py
+++ generic_positions/tests/test_app/models.py
@@ -9,8 +9,5 @@ class DummyModel(models.Model):
     """Dummy to be used in test cases of the ``generic_positions`` app."""
     name = models.CharField(max_length=256, blank=True)
 
-    class Meta:
-        ordering = ['generic_position__position']
-
 DummyModel.add_to_class(
     'generic_position', generic.GenericRelation(ObjectPosition))

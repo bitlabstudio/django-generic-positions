1f5510c005da0f768ee2db1a55c6206936b4c576
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2016-04-18 21:18:58 +0200

    removed tox folder

--- .tox/py27-django18/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Tyrdall/projects/django-generic-positions/src/.tox/py27-django18/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

8edd045be0a3b43e5d88b855d7e0e42aae7411d0
Tobias Lorenz <tobias.lorenz@rtl.de>
2026-05-14 17:57:16 +0200

    fix: fixed ugettext imports

--- generic_positions/models.py
+++ generic_positions/models.py
@@ -1,8 +1,9 @@
 """Models for the ``generic_positions`` app."""
+
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import fields
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ObjectPosition(models.Model):
@@ -13,6 +14,7 @@ class ObjectPosition(models.Model):
     :position: Current position integer of the object.
 
     """
+
     # Generic Foreign Key Bundle
     content_type = models.ForeignKey(
         ContentType,
@@ -25,8 +27,9 @@ class ObjectPosition(models.Model):
 
     # Other fields
     position = models.PositiveIntegerField(
-        verbose_name=_('Position'),
-        null=True, blank=True,
+        verbose_name=_("Position"),
+        null=True,
+        blank=True,
     )
 
 
@@ -42,9 +45,9 @@ def save_positions(post_data, queryset=None):
     if not queryset:
         queryset = ObjectPosition.objects.all()
     for key in post_data:
-        if key.startswith('position-'):
+        if key.startswith("position-"):
             try:
-                obj_id = int(key.replace('position-', ''))
+                obj_id = int(key.replace("position-", ""))
             except ValueError:
                 continue
             queryset.filter(pk=obj_id).update(position=post_data[key])
